026905b8fca6a15f9fa3ddf67e8972d5ca6d473a
samhsiao6238 <samhsiao6238@gmail.com>
2024-04-12 01:46:24 +0800

    更新

---
+++ D21_Vercel/00_專案/01_基礎/api/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+
+import os  # 導入 os 模組
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+# 從環境變數中獲取 LineBot 的設置
+configuration = Configuration(access_token=os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info(
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
+        abort(400)
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)],
+            )
+        )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
