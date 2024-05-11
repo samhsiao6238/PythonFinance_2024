7f1dcbaa75368ba1b4f2793c4fb86e7c19015e0f
samhsiao6238 <samhsiao6238@gmail.com>
2024-05-12 03:25:38 +0800

    s989

---
+++ D21_neo4j/03_st-neo4j-bot/README.md
@@ -0,0 +1,210 @@
+# 說明
+
+1. 這個專案主要包含了三個腳本：`app.py`、`cypher_chain.py`、`cypher_validator.py`。
+
+2. 以下將分別解說這三個腳本。
+
+<br>
+
+## `cypher_chain.py`
+
+1. 充分利用 `langchain` 庫來整合文字處理和圖資料庫查詢的流程，透過自訂的鍊式操作處理使用者輸入，產生回應的 Cypher 查詢，並與 Neo4j 資料庫互動。
+
+2. 模組的設計旨在透過自然語言處理增強資料庫查詢的生成和執行過程，實現了從問題解析到資料庫查詢的自動化。
+
+<br>
+
+### 導入庫
+
+1. **導入必要的庫**：
+
+    - `os`：用於作業系統功能，如讀取環境變數。
+
+    - `typing`：提供類型支援。
+
+    - `langchain`：一個函式庫，用於鍊式處理語言相關的任務，包括產生Cypher查詢。
+
+    - `pydantic`：用於資料驗證和設定管理。
+
+<br>
+
+2. **條件導入**：
+    - 嘗試從 `pydantic.v1.main` 匯入 `BaseModel` 和 `Field`，如果失敗則從 `pydantic.main` 匯入，這是向後相容的處理方式。
+
+<br>
+
+### 自訂函數 `remove_entities`
+
+- **目的**：將文字中的命名實體替換為它們對應的實體標籤。
+
+- **實作方式**：
+
+    - 遍歷文件的實體（`doc.ents`），以實體的標籤（`ent.label_`）取代實體文字。
+
+    - 在實體前後加入非實體文字。
+
+    - 返回修改後的文字。
+
+<br>
+
+### 預設的關係和Cypher模板
+
+- **定義實體間的可能關係**：列舉了可用於建構查詢的實體間關係，例如人與人之間的親子關係、組織與組織之間的供應關係等。
+
+- **Cypher系統模板**：定義了一個模板字串，描述了使用者角色，將使用者問題轉換為準確的Cypher查詢。
+
+<br>
+
+### `CustomCypherChain` 類
+
+- **實現詳細功能**：
+
+    - **處理實體**：從文字中擷取組織和人員實體。
+
+    - **產生視覺化資料**：建構與實體相關的查詢，產生視覺化資料。
+
+    - **尋找實體符合**：在資料庫中尋找符合的實體。
+
+    - **產生系統訊息**：根據相關實體和範例產生系統訊息，指導產生Cypher查詢。
+
+    - **取得範例查詢**：使用向量搜尋取得與問題相關的Cypher查詢範例。
+
+<br>
+
+- **主呼叫方法 `_call`**：
+
+    - 從輸入中提取問題和聊天歷史。
+
+    - 處理文字中的實體，符合資料庫中的實體。
+
+    - 取得幾個範例查詢，產生系統訊息。
+
+    - 使用鍊式模型產生Cypher查詢。
+
+    - 執行查詢，處理結果並建立最終輸出。
+
+<br>
+
+
+
+## cypher_validator.py
+
+1. 定義了一個 `CypherQueryCorrector` 的類，主要功能是校正產生的 Cypher 查詢語句，確保它們符合預先定義的圖資料庫模式和關聯方向。
+
+2. `CypherQueryCorrector` 類中透過一系列的正規比對和邏輯判斷，對產生的 Cypher 查詢進行檢查和修改，以確保查詢符合特定的圖資料庫模式和關係方向，這對於使用 AI 模型自動產生資料庫查詢語句的應用尤其重要，因為模型產生的內容可能不完全符合資料庫的實際結構和約束。
+
+<br>
+
+### 導入
+
+- **匯入必要的模組**：
+
+    - 導入 `re` 用於正規表示式操作。
+
+    - `collections.namedtuple` 用於建立命名元組。
+
+    - `typing` 提供類型註解。
+
+<br>
+
+- **定義`Schema`**：
+
+    - 使用 `namedtuple` 建立一個名為`Schema` 的資料結構，包含`left_node`、`relation`、`right_node` 三個欄位，用於描述資料庫中的實體（節點）和它們之間的關係。
+
+<br>
+
+### 類別定義：`CypherQueryCorrector`
+
+- **類別說明**：
+   - 此類別用於校正 AI 模型產生的 Cypher 查詢，主要是關係方向的校正。
+
+<br>
+
+- **正規表示式模式**：
+
+   - `property_pattern`：符合節點或關係的屬性。
+
+   - `node_pattern`：符合節點定義。
+
+   - `path_pattern`：符合包含節點和關係的完整路徑。
+
+   - `node_relation_node_pattern`：更詳細地符合形式為 (節點)-[關係]->(節點) 的路徑。
+
+   - `relation_type_pattern`：符合關係型別及其屬性。
+
+<br>
+
+- **初始化方法 (`__init__`)**：
+
+   - 接收一個 `schemas` 參數，清單中包含多個 `Schema` 實例，定義了資料庫中允許的節點和關係模式。
+
+<br>
+
+### 功能性方法
+
+- **`clean_node`**：清理節點字串，去除括號和屬性，只留下節點的基本表示。
+
+- **`detect_node_variables`**：從查詢中提取所有節點，並將它們的變數名稱和類型標籤整理成字典。
+
+- **`extract_paths`**：從查詢中提取所有包含節點和關係的完整路徑。
+
+- **`judge_direction`**：判斷關係的方向（入射、出射或雙向）。
+
+- **`extract_node_variable`**：從節點描述中提取變數名。
+
+- **`detect_labels`**：根據節點變數字典，從節點描述中提取標籤。
+
+- **`verify_schema`**：驗證給定的節點標籤和關係類型是否符合預先定義的 `schemas`。
+
+- **`detect_relation_types`**：從關係描述中提取關係類型和方向。
+
+- **`correct_query`**：校正查詢語句中的關係方向，確保它們符合 `schemas` 定義的正確方向和類型。
+
+<br>
+
+### 呼叫方法 (`__call__`)
+
+- **使類別實例可以像函數一樣被呼叫**：接收一個 Cypher 查詢，呼叫 `correct_query` 方法進行校正，傳回校正後的查詢。
+
+<br>
+
+
+## 關於容器設定
+
+1. 當使用 VSCode 的 Remote Containers 外掛程式或 GitHub Codespaces 運行容器化的開發環境時，VSCode 預設會嘗試使用 `gio` 命令開啟一個外部瀏覽器視窗以存取 Streamlit 應用程式，但在不支援 GUI 或 `gio` 指令的環境中會遇到問題，而 `容器環境` 就是其一。
+
+    ```bash
+    # 會顯示警告
+    gio: http://localhost:8502: Operation not supported
+    ```
+
+<br>
+
+2. 可透過修改 `devcontainer.json` 文件中的服務啟動指令，加入 `無頭模式` 設定，也就是不會嘗試自動開啟瀏覽器視窗。
+
+    ```json
+    "postAttachCommand": "streamlit run src/app.py --server.enableCORS false --server.enableXsrfProtection false --server.headless true"
+    ```
+
+<br>
+
+3. 補充說明這個命令的伺服器選項：
+
+    - `--server.enableCORS false`：停用跨來源資源共享（CORS）。在開發環境中，如果前端和後端在同一台主機上，可能不需要 CORS 支援。
+
+    - `--server.enableXsrfProtection false`：停用跨站請求偽造（XSRF）保護。通常在處理特定類型的跨站請求時可以關閉，但這可能會降低安全性。
+
+    - `--server.headless true`：確保 Streamlit 在無頭模式下運行，不嘗試開啟瀏覽器視窗。
+
+<br>
+
+
+## 資料庫
+
+1. 透過 [官網](https://demo.neo4jlabs.com:7473/browser/) 訪問資料庫以確認 URL、帳號、密碼都正確。
+
+<br>
+
+___
+
+_END_
---
+++ D21_neo4j/03_st-neo4j-bot/src/app.py
@@ -0,0 +1,212 @@
+import os
+from typing import List, Union
+
+import streamlit as st
+import graphviz
+# 棄用或不適用
+# from langchain.chat_models import ChatOpenAI
+# from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
+from langchain.graphs import Neo4jGraph
+from langchain.schema import HumanMessage, AIMessage
+
+# 自訂函數
+from cypher_chain import CYPHER_QA_PROMPT
+from cypher_chain import CustomCypherChain
+
+
+# 標題
+st.title("VC Chatbot")
+
+
+url = st.secrets["NEO4J_URI"]
+username = st.secrets["NEO4J_USERNAME"]
+password = st.secrets["NEO4J_PASSWORD"]
+database = st.secrets["NEO4J_DATABASE"]
+
+# Langchain x Neo4j connections
+graph = Neo4jGraph(username=username, password=password, url=url, database=database)
+
+graph_search = None
+
+# Session state
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+
+if "user_input" not in st.session_state:
+    st.session_state["user_input"] = []
+
+if "viz_data" not in st.session_state:
+    st.session_state["viz_data"] = []
+
+if "database" not in st.session_state:
+    st.session_state["database"] = []
+
+if "cypher" not in st.session_state:
+    st.session_state["cypher"] = []
+
+
+# 自訂函數，生成回答文本，至少要傳入一個參數 `文本`
+def generate_context(
+    prompt: str, context_data: str = "generated"
+) -> List[Union[AIMessage, HumanMessage]]:
+    context = []
+    # 假如存在查詢歷史紀錄
+    if st.session_state["generated"]:
+        # 加入最近的三筆查詢
+        size = len(st.session_state["generated"])
+        for i in range(max(size - 3, 0), size):
+            context.append(HumanMessage(content=st.session_state["user_input"][i]))
+            context.append(AIMessage(content=st.session_state[context_data][i]))
+    # 加入本次的使用者查詢語句 `prompt`
+    context.append(HumanMessage(content=str(prompt)))
+    # 將處理後的文本列表傳出
+    return context
+
+
+# 用於創建並顯示 st 應用中的動態響應標籤，參數 `i` 當前處理對話的索引
+def dynamic_response_tabs(i):
+    # 初始化一個標籤列表，用於展示雙方對話
+    tabs_to_add = ["💬Chat"]
+    # 根據 `st.session_state` 檢查數據類型是否存在
+    data_check = {
+        "🔍Cypher": st.session_state["cypher"][i],
+        "🗃️Database results": st.session_state["database"][i],
+        "🕸️Visualization": st.session_state["viz_data"][i]
+        # 進一步檢查 `viz_data` 是否有具體可用的可視化數據
+        and st.session_state["viz_data"][i][0],
+    }
+    # 遍歷 `data_check`
+    for tab_name, has_data in data_check.items():
+        # 假如存在數據，則加入對話列表中 `tabs_to_add`
+        if has_data:
+            tabs_to_add.append(tab_name)
+    # 使用 `with` 實現上下文管理器 `st.chat_message` 來顯示用戶輸入的訊息
+    # 在這個區塊中，所有對話都會以氣泡顯示
+    # `user` 代表的是顯示在用戶側
+    with st.chat_message("user"):
+        # 展示用戶的輸入
+        st.write(st.session_state["user_input"][i])
+    # 使用上下文管理器展示助手的回覆
+    with st.chat_message("assistant"):
+        # 建立一組標籤頁，每個標籤頁對應一個 `tabs_to_add`
+        selected_tabs = st.tabs(tabs_to_add)
+        # 根據不同的標籤頁來顯示不同的訊息
+        with selected_tabs[0]:
+            # 展示聊天內容
+            st.write(st.session_state["generated"][i])
+        if len(selected_tabs) > 1:
+            with selected_tabs[1]:
+                # 展示 Cypher 查詢代碼
+                st.code(st.session_state["cypher"][i], language="cypher")
+        if len(selected_tabs) > 2:
+            with selected_tabs[2]:
+                # 顯示資料庫查詢結果
+                st.write(st.session_state["database"][i])
+        if len(selected_tabs) > 3:
+            with selected_tabs[3]:
+                # 創建一個圖，並添加節點和邊
+                graph_object = graphviz.Digraph()
+                for final_entity in st.session_state["viz_data"][i][1]:
+                    graph_object.node(
+                        final_entity, fillcolor="lightblue", style="filled"
+                    )
+                for record in st.session_state["viz_data"][i][0]:
+                    graph_object.edge(
+                        record["source"], record["target"], label=record["type"]
+                    )
+                st.graphviz_chart(graph_object)
+
+
+# 自訂函數：取得使用者的輸入
+def get_text() -> str:
+    # 輸入框 
+    input_text = st.chat_input("誰是 Neo4j 的 CEO？")
+    # 假如還沒輸出，檢查的邏輯是判斷有沒有以 `sk-` 開頭的字串
+    if not openai_api_key.startswith("sk-"):
+        # 標題之下會提示輸入
+        st.warning("請輸入你的 OpenAI API key!", icon="⚠")
+    else:
+        return input_text
+
+
+try:
+    # 從 `sidebar` 框內取得
+    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+    # 寫入環境參數，這在程序內運行，有生命週期，不會外洩
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    # 假如 True
+    if openai_api_key:
+        # 這是主要查詢的邏輯，使用圖形數據庫查詢鏈 `CustomCypherChain` 來建立實體
+        graph_search = CustomCypherChain.from_llm(
+            # 預設使用 `gpt-4-turbo`
+            # temperature 範圍 0~1，越低代表越精準，這是自然語言領域中常見的一個參數
+            # 用於生成 Cypher 查詢
+            cypher_llm=ChatOpenAI(temperature=0.0, model_name="gpt-4-turbo"),
+            # 用於處理與查詢相關的其他文本生成任務
+            qa_llm=ChatOpenAI(temperature=0.0),
+            # 用於執行生成的 Cypher 查詢
+            graph=graph,
+            # 用於生成查詢的提示模板，通過指定的格式引導 `qa_llm` 生成有用的回答
+            qa_prompt=CYPHER_QA_PROMPT,
+        )
+
+    # 這是顯示在左側的文本
+    st.sidebar.markdown(
+        """
+    ## Example questions
+
+    * What do you know about Neo4j organization?
+    * Who is Neo4j CEO?
+    * How is Emil Eifrem connected to Magnus Christerson?
+    * Which company has the most subsidiaries?
+    * Who are the competitors of databricks?
+    * How many levels of subsidiaries does Blackstone has?
+    * Are there any news regarding employee satisfaction?
+    * What are the latest news around companies where Emil Eifrem is CEO?
+    * Are there any news about new partnerships mentioning Neo4j?
+
+    You can also ask follow up questions as we use a conversational LLM under the hood.
+
+    Code is available on [GitHub](https://github.com/tomasonjo/streamlit-neo4j-hackathon)
+    """
+    )
+
+    # 調用自訂函數取得輸入框中使用者輸入的文本，這裡調用的函數僅做檢查，並未處理文本
+    user_input = get_text()
+    # 非零為 True
+    if user_input:
+        # 先顯示 `進行中`
+        with st.spinner("Processing"):
+            # 透過自訂函數處理使用者輸入的文本，主要是添加歷史查詢
+            context = generate_context(user_input)
+            # `graph_search` 是一個 `CustomCypherChain.from_llm` 物件
+            # 傳入使用者輸入與歷史查詢
+            print(f'user_input={user_input}')
+            print(f'chat_history={context}')
+            output = graph_search({"query": user_input, "chat_history": context})
+            # session_state 是一個持久化儲存技術，將這些跨頁面的數據可以用字典的型態被保存
+            # 儲存當前對話
+            st.session_state.user_input.append(user_input)
+            # 儲存結果
+            st.session_state.generated.append(output["result"])
+            # 儲存可能生成的可視化數據
+            st.session_state.viz_data.append(output["viz_data"])
+            # 儲存與當前查詢相關的資料庫訊息
+            st.session_state.database.append(output["database"])
+            # 儲存生成的 Cypher 查詢（Query）
+            st.session_state.cypher.append(output["cypher"])
+
+
+    if st.session_state["generated"]:
+        # 先計算鍵值 `generated` 的資料個數
+        size = len(st.session_state["generated"])
+        # 顯示最後三次互動
+        for i in range(max(size - 3, 0), size):
+            #
+            dynamic_response_tabs(i)
+except:
+    print('=發生例外狀況=')
+finally:
+    #
+    print('=try 結束=')
