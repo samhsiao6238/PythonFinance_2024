f8237156e673a7541b7d2e374d8de51b3a86c59b
samhsiao6238 <samhsiao6238@gmail.com>
2024-05-13 23:50:33 +0800

    s111

--- D21_neo4j/00_基本操作與介紹/00_腳本/app.py
+++ D21_neo4j/00_基本操作與介紹/00_腳本/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from utility import get_neo4j_session, load_movie_data
-# 載入 dotenv
+from utility import get_neo4j_driver, load_movie_data
 from dotenv import load_dotenv
 import os
 
@@ -17,21 +16,24 @@ PASSWORD = os.getenv("NEO4J_PASSWORD")
 def main():
     st.title("Neo4j Movie Data")
 
-    # 連接到資料庫
-    session = get_neo4j_session(URI, USER, PASSWORD)
+    # 創建驅動實例
+    driver = get_neo4j_driver(URI, USER, PASSWORD)
 
-    # 載入數據
-    movies = load_movie_data(session)
+    try:
+        # 載入數據
+        movies = load_movie_data(driver)
 
-    # 顯示數據
-    if movies:
-        for movie in movies:
-            st.write(f"Title: {movie['title']}, Released: {movie['released']}")
-    else:
-        st.write("No data found.")
-
-    # 關閉資料庫對話
-    session.close()
+        # 顯示數據
+        if movies:
+            for movie in movies:
+                st.write(
+                    f"Title: {movie['title']}, Released: {movie['released']}"
+                )
+        else:
+            st.write("No data found.")
+    finally:
+        # 確保無論如何都會關閉驅動器
+        driver.close()
 
 
 if __name__ == "__main__":
--- D21_neo4j/00_基本操作與介紹/00_腳本/utility.py
+++ D21_neo4j/00_基本操作與介紹/00_腳本/utility.py
@@ -1,20 +1,24 @@
 from neo4j import GraphDatabase
 
 
-# 連接到 Neo4j 資料庫
-def get_neo4j_session(uri, user, password):
+# 創建 Neo4j 資料庫驅動實例
+def get_neo4j_driver(uri, user, password):
     driver = GraphDatabase.driver(uri, auth=(user, password))
-    return driver.session()
+    return driver
 
 
-# 載入電影數據
-def load_movie_data(session):
-    query = "MATCH (m:Movie) RETURN m.title AS title, m.released AS released LIMIT 10"
-    result = session.run(query)
-    movies = [
-        {
-            "title": record["title"],
-            "released": record["released"]
-        } for record in result
-    ]
+# 進行資料查詢
+def load_movie_data(driver):
+    with driver.session() as session:
+        query = """
+        MATCH (m:Movie)
+        RETURN m.title AS title, m.released AS released LIMIT 10
+        """
+        result = session.run(query)
+        movies = [
+            {
+                "title": record["title"],
+                "released": record["released"]
+            } for record in result
+        ]
     return movies
